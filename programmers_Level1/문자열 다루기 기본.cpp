f5b1da1b5fc35178504fc7ee156d5729b16d7601
shhmm <78362004+shhmm@users.noreply.github.com>
2021-02-02 12:18:50 +0900

    문자열 다루기 기본

---
+++ programmers_Level1/문자열 다루기 기본.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = false;
+    if(s.length() == 4 || s.length() == 6){
+        answer = true;
+        for(int i=0;i < s.length(); i++){
+            if(s[i] < '0' || s[i] > '9'){
+                answer = false;
+                break;
+            }
+        }
+    }
+    return answer;
+}
